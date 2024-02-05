46a50f6250f1ead2c525d942984cff96382cd0fd
Lya Roux <lya.roux@pm.me>
2024-02-05 21:19:06 +0100

    handled exceptions

--- nerdle.py
+++ nerdle.py
@@ -19,35 +19,60 @@ def newEquation():
 eq = str(newEquation())
 # print(eq, len(eq))
 guesses = 0
+allowedchar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '=', '+', '-', '*']
 
-while guesses<5:
+class notAnEquation(Exception):
+    "Raised when the guess is not an equation"
+    pass
+class lettersInGuess(Exception):
+    "Raised when there are something other than numbers and operators in the guess"
+    pass
+
+def main(guesses):
 
     feedback = []
-
     for i in range(len(eq)):
         print("_", end="")
         feedback.append(0)
     print("")
-    
+
     answer = input()
+
+    if len(eq) != len(answer):
+        raise IndexError
+    elif '=' not in answer:
+        raise notAnEquation
+    
     for i in range(len(answer)):
+        if answer[i] not in allowedchar:
+            raise lettersInGuess
         if answer[i] in eq:
             feedback[i]=1
         if answer[i] == eq[i]:
-           feedback[i]=2
+            feedback[i]=2
+                
 
     if 0 not in feedback and 1 not in feedback:
         print("You win !")
-        exit()    
-    else:
-        guesses+=1
-        pass
-
-    if guesses >=5:
-        print("perdu !")
         exit()
+    else:
+        for i in feedback:
+            print(i, end='')
+        print("", end="\n\n")
+        return guesses+1
 
-    for i in feedback:
-        print(i, end='')
-    print("", end="\n\n")
 
+
+while guesses<5:
+    try:
+        guesses = main(guesses)
+    except IndexError:
+        print("Guess length not valid !")
+        pass
+    except notAnEquation:
+        print("Guess must be an equation !")
+        pass
+    except lettersInGuess:
+        print("Guess must contain only ints and operators, except / and % !")
+        pass
+print("You lost !")
